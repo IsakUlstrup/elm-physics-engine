5e9b042c8f2d504034eb9b5d2332a523e03d2c57
Isak <isakulstrup@gmail.com>
2024-09-30 13:52:50 +0200

    added damping

--- src/Engine/Particle.elm
+++ src/Engine/Particle.elm
@@ -7,9 +7,10 @@ type alias Particle =
     { position : Vector
     , velocity : Vector
     , acceleration : Vector
+    , damping : Float
     }
 
 
 new : Vector -> Particle
 new position =
-    Particle position Vector.zero Vector.zero
+    Particle position Vector.zero Vector.zero 0.995
